7ff9d99a0d553e852b81d11c373f7ea9efce65db
kamui-akatuki <kamui-akatuki@kamui-akatukinoMacBook-Air.local>
2024-09-20 15:18:12 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,18 @@
 #![no_main]
 
 // pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use panic_halt as _; // パニックをキャッチするために`rust_begin_unwind`にブレークポイントを置くことができます
+                     // use panic_abort as _; // requires nightly
+                     // use panic_itm as _; // ITM 経由でメッセージをログに記録します。ITM サポートが必要です。
+                     // use panic_semihosting as _; // メッセージをホストのstderrに記録します。デバッガが必要です。
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
+use stm32f4::stm32f446::{interrupt, Interrupt, NVIC};
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    asm::nop(); // リリースモードでメインの最適化を中止しないようにするには、コードを追加するときに削除します。
 
     loop {
         // your code goes here
